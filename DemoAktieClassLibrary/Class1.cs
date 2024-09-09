02c5ee5fd0c796d9d05af3c7c3669546c77fb57e
MARK <MARK@Z4087>
2024-09-09 13:58:15 +0200

    Add project files.

---
+++ DemoAktieClassLibrary/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAktieClassLibrary
+{
+    /// <summary>
+    /// Klasse der indeholder aktioeoplysninger
+    /// </summary>
+    public class Aktie
+    {
+        /// <summary>
+        /// navnet på aktien
+        /// </summary>
+        public string Navn { get; set; }
+
+        /// <summary>
+        /// constructor 
+        /// </summary>
+        /// <param name="navn">navnet på aktien</param>
+        public Aktie(string navn)
+        {
+            this.Navn = navn;
+        }
+
+    }
+}
